7b882db32ed4c5f2e25615ab69f600b4c9b59bf3
angelbdr02 <abd0019@alu.medac.es>
2024-04-25 13:07:16 +0200

    Feature2 Tercer Commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Feature2 Segundo Commit");
+        System.out.println("Feature2 Tercer Commit");
     }
 }
