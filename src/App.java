d53d34a2905b8aa939d79a1038716fec1e4e53a6
angelbdr02 <abd0019@alu.medac.es>
2024-04-25 13:06:46 +0200

    Feature2 Primer Commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Develop Primer commit");
+        System.out.println("Feature2 Primer Commit");
     }
 }
