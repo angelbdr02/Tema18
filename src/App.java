d0394ff70a580eb1bfce6dc3aa57074e41771f49
angelbdr02 <abd0019@alu.medac.es>
2024-04-25 13:12:16 +0200

    4 commit develop

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Hotfix!");
+        System.out.println("Develop 4 commit");
     }
 }
