b2a11d941b8d6287a8724fe0ce36bdce9cf7a9ca
angelbdr02 <abd0019@alu.medac.es>
2024-04-25 13:13:56 +0200

    Release commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Develop 5 commit");
+        System.out.println("Commit release");
     }
 }
