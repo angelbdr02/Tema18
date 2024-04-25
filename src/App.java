5c137a59d4de89d0dd7774881d42dcaabff97224
angelbdr02 <abd0019@alu.medac.es>
2024-04-25 15:27:37 +0200

    MASTER FINAL

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Commit release");
+        System.out.println("MASTER FINAL");
     }
 }
